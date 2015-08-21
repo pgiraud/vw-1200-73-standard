5d7993474405f9282b2bba1808f421d03a9c0cbf
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-08-21 23:36:49 +0200

    Baguette instead of lightbox

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 $(function() {
-  var count = 0;
   $('.post-content img').each(function() {
-    var set = 'set' + count;
-    var el = $(this);
-    if (el.next().prop('tagName') != 'IMG') {
-      count++;
-    }
     $(this).wrap($('<a>', {
-      href: $(this).attr('src').replace(/_[a-z]\.jpg/, '_b.jpg'),
+      href: $(this).attr('src').replace(/_[a-z]\.jpg/, '_s.jpg'),
       'class': 'image',
-      'data-lightbox': set,
-      'data-title': $(this).attr('alt')
+      'data-caption': $(this).attr('alt'),
+      'data-at-450': $(this).attr('src').replace(/_[a-z]\.jpg/, '_z.jpg'),
+      'data-at-800': $(this).attr('src').replace(/_[a-z]\.jpg/, '_c.jpg'),
+      'data-at-1366': $(this).attr('src').replace(/_[a-z]\.jpg/, '_b.jpg')
     }));
   });
+  baguetteBox.run('.post-content', {});
 });
