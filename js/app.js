4d9ea8041d67684c131bdd5015fa05c495867ae3
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-08-22 09:25:18 +0200

    Smaller images on mobile devices

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 $(function() {
   $('.post-content img').each(function() {
     $(this).wrap($('<a>', {
-      href: $(this).attr('src').replace(/_[a-z]\.jpg/, '_s.jpg'),
+      href: $(this).attr('src').replace(/_[a-z]\.jpg/, '_q.jpg'),
       'class': 'image',
       'data-caption': $(this).attr('alt'),
       'data-at-450': $(this).attr('src').replace(/_[a-z]\.jpg/, '_z.jpg'),
       'data-at-800': $(this).attr('src').replace(/_[a-z]\.jpg/, '_c.jpg'),
       'data-at-1366': $(this).attr('src').replace(/_[a-z]\.jpg/, '_b.jpg')
     }));
+
+    if ($(this).attr('src').indexOf('_q') != -1) {
+      $(this).addClass('thumbnail');
+    }
   });
   baguetteBox.run('.post-content', {});
 });
